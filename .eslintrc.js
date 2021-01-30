2ec4a726f686cf7be99efa25f75a09ad79769270
khriztianmoreno <khriztianmoreno@gmail.com>
2021-01-30 09:09:46 -0500

    Install express

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,13 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard'],
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 12,
   },
   rules: {
-  }
+    'space-before-function-paren': 'never',
+  },
 }
